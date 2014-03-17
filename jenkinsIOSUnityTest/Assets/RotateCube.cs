6597060f79683fbd8b65706fa459af162fe4b37e
Kandu Maker <kandu.test@gmail.com>
2014-03-17 15:22:05 -0400

    init
    initial commit for auto build test

---
+++ jenkinsIOSUnityTest/Assets/RotateCube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+
+	float rot = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate (0f,rot,0f);
+	}
+}
