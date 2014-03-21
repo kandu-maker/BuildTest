58f350bfc04b35e61c721471a7350a50a5ae2c54
David Epstein <dave@eppygames.com>
2014-03-20 21:32:29 -0400

    -!

--- jenkinsIOSUnityTest/Assets/Test.cs
+++ jenkinsIOSUnityTest/Assets/Test.cs
@@ -5,7 +5,7 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("!!!!!!!!!!!My New Test!!!!!!!!!!!!");
+		Debug.Log ("!!My New Test!!!!!!!!!!!!");
 	}
 	
 	// Update is called once per frame
